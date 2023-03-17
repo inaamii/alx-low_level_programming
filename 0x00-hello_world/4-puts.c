413bc8ac44a5019d1e9e0c5e0fea9ca51d6d1b9d
inaamii <ismailnaami2028@gmail.com>
2023-03-17 02:40:48 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /* main that prints exactly "Programming is like building a multilingual puzzle  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
