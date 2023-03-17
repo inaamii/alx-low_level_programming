f4c4ba8d96cc4cd5b93f4c27d2db8575b4b17f1a
inaamii <ismailnaami2028@gmail.com>
2023-03-17 02:18:12 +0100

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* 
+program that prints exactly with proper grammar, but the outcome is a piece of art */
+
+int main(void)
+{
+   puts ("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
