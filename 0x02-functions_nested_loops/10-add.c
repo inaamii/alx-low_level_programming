9a9bfbfed9b3b623b5251eb141d5a0ead0b5a94d
inaamii <ismailnaami2018@gmail.com>
2023-03-22 11:09:30 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - a function that adds tow integers and return the result
+ *
+ * @a: first integer
+ * @b: one more integer
+ *
+ * Return: result of sum
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+		return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * times_table - a function that print the 9 time table
+ * 			starting with 0
+ */
+
+void times_table(void)
+{
+
+}
