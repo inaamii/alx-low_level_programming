154f9885d6b43f71d31190bfcb134aa563c9a59a
inaamii <ismailnaami2018@gmail.com>
2023-03-22 17:37:19 -0700

    0 > 972?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,9 @@
-carrie@ubuntu:/debugging$ cat main.h
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int , int , int );
 
-#endif /* MAIN_H *
+#endif /* MAIN_H */
