8ee54932dec7582ee216f01c8cfcf11c9862cdd7
inaamii <ismailnaami2018@gmail.com>
2023-03-27 18:39:36 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - a a function that prints every other
+ * character of a string,
+ *
+ * @str : string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
