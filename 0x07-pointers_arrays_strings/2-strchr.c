f53e0204a16afb6134f0b517895eeeefc1083e95
inaamii <ismailnaami2018@gmail.com>
2023-04-03 16:27:00 -0700

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,10 +15,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] >= '\n'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
+		if (s[i] == 0)
+			return (NULL);
 	}
 	return ('\0');
 
