01aba2f5a715ecfe09df4c2c9cec7f5d99bd1b0a
inaamii <ismailnaami2018@gmail.com>
2023-04-09 15:37:46 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,16 +1,29 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - a program that print the number of arguments
+ * main - a program that multiplies two numbers.
  *
  * @argv : arguments numbers
  * @argc : array of arguments
  *
- * Return: 0
+ * Return: if the program does not receive tow argument
+ * return Error, else 0
  */
 
 int main(int argc, char *argv[])
 {
+	int mul;
 
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+	}
+	else
+	{
+	mul = (atoi(argv[1]) * atoi(argv[2]));
+	printf("%d\n", mul);
+	}
+	return (0);
 }
