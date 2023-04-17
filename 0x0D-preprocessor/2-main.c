4a07be19181d07596d740f8419773d53552a5292
inaamii <ismailnaami2018@gmail.com>
2023-04-17 10:42:54 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - a program that prints the name of the file it was compiled from
+ *
+ * Return: 0
+ */
+
+int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
