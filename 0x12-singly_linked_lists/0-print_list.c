56ae88cef6a4fff832501fa60b3a47bf7d0516b7
inaamii <ismailnaami2018@gmail.com>
2023-04-27 17:58:45 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,13 +15,9 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[%d] (nil)\n", 0);
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
 		h = h->next;
 	NUM++;
 	}
