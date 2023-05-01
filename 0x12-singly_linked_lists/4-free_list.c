474f153236282df461e772b835462b07b35510e4
inaamii <ismailnaami2018@gmail.com>
2023-04-30 18:02:38 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - a function that frees a list_t list.
+ *
+ * @head : list taht freed
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t swp;
+
+	while (head != '\0')
+	{
+		swp = head->next;
+		free(head->str);
+		free(head);
+		head = swp;
+	}
+}
